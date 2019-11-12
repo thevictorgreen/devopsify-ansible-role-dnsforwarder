868ac7a95d011ca3cbc1682890efdba74b9db4d0
thevictorgreen <thevictorgreen@gmail.com>
2019-11-12 09:02:21 -0500

    cfgmgt_up_2

---
+++ files/bind_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# LOG OUTPUT TO A FILE
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/root/.bind_automate/log.out 2>&1
+
+if [[ ! -f "/root/.bind_automate/init.cfg" ]]
+then
+  # COPY CONFIG FILE
+  cp named.conf.options /etc/bind/
+  # RESTART bind9
+  systemctl restart bind9
+  # CHECK BIND STATUS
+  systemctl status bind9
+  sleep 60
+  touch /root/.bind_automate/init.cfg
+fi
